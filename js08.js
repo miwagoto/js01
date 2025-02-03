153e5d747bc7c65eda8c2f6346cb926ac3d84530
miwa <ec2-user@ip-172-31-38-97.ap-northeast-1.compute.internal>
2025-02-03 08:35:50 +0000

    課題08

---
+++ js08.js
@@ -0,0 +1,14 @@
+function area(base, height) {
+    let S = 0.5 * base * height;
+    return S;
+  }
+console.log(area(10, 20));
+
+  
+var area = function(base, height) {
+  let S = 0.5 * base * height;
+  return S;
+  }
+console.log(area(10, 20));
+
+  
